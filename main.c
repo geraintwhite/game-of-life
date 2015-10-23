50a44f771b783d960a19a0503a4ad013e7f5d464
Geraint White <geraintwhite@gmail.com>
2015-10-23 21:43:03 +0100

    Add second circle

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 int
 main(int argc, char * argv[])
 {
-  int height, width;
+  int height, width, radius;
 
   initscr();
   curs_set(0);
@@ -25,18 +25,27 @@ main(int argc, char * argv[])
 
   printw("%d, %d", height, width);
 
+  radius = (2 * height > width ? width / 2 : height) / 2;
+
   Point centre;
   centre.y = height / 2;
   centre.x = width / 2;
 
-  Circle circle;
-  circle.radius = (2 * height > width ? width / 2 : height) / 2;
-  circle.centre = centre;
+  Circle circle1;
+  circle1.radius = radius;
+  circle1.centre = centre;
+
+  Circle circle2;
+  circle2.radius = radius / 2;
+  circle2.centre = centre;
 
   init_pair(1, COLOR_RED, COLOR_RED);
+  init_pair(2, COLOR_YELLOW, COLOR_YELLOW);
+
   attron(COLOR_PAIR(1));
-  draw_circle(circle, DOT);
-  attroff(COLOR_PAIR(1));
+  draw_circle(circle1, DOT);
+  attron(COLOR_PAIR(2));
+  draw_circle(circle2, DOT);
 
   refresh();
   getch();
