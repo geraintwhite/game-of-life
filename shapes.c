493b92b2e218e3410d2d0be8b0ff7313da66aa98
Geraint White <geraintwhite@gmail.com>
2015-11-06 12:25:13 +0000

    Add get_points function to populate array with coords of circle

--- shapes.c
+++ shapes.c
@@ -1,8 +1,10 @@
+#include "shapes.h"
+#include "maths.h"
+
 void
-draw_circle(Circle circle, char dot)
+get_points(Circle circle, Point * points)
 {
-  float deg;
-  int y, x, height, width;
+  int y, x, height, width, deg;
 
   height = circle.radius;
   width = 2 * circle.radius;
@@ -13,9 +15,21 @@ draw_circle(Circle circle, char dot)
 
   for (deg = 0; deg < 360; deg++)
   {
-    y = (int) offset.y + height + (height * sin(DEGTORAD(deg)));
-    x = (int) offset.x + width + (width * cos(DEGTORAD(deg)));
+    points[deg].y = (int) offset.y + height + (height * sin(DEGTORAD(deg)));
+    points[deg].x = (int) offset.x + width + (width * cos(DEGTORAD(deg)));
+  }
+}
 
-    mvaddch(y, x, dot);
+void
+draw_circle(Circle circle, char dot)
+{
+  int i;
+  Point * points = malloc(sizeof(Point) * 360);
+
+  get_points(circle, points);
+
+  for(i = 0; i < 360; i++)
+  {
+    mvaddch(points[i].y, points[i].x, dot);
   }
 }
