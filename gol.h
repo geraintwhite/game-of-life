10b64f29fbae220f08761248ef6d955ba3aee6a7
Geraint White <geraintwhite@gmail.com>
2015-11-06 15:38:20 +0000

    Add game-of-life logic

--- gol.h
+++ gol.h
@@ -2,7 +2,6 @@
 
 #define COORD(y, x) ((y) * COLS + (x))
 #define CELL(y, x) (cells[COORD((y), (x))])
+#define BUFFER(y, x) (buffer[COORD((y), (x))])
 
 #define ALIVE(y, x) (CELL((y), (x)))
-#define LIVE(y, x) (cells[COORD((y), (x))] = 1)
-#define KILL(y, x) (cells[COORD((y), (x))] = 0)
--- main.c
+++ main.c
@@ -1,43 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <ncurses.h>
 
-#include "gol.h"
 #include "shapes.c"
+#include "gol.h"
 
 
 static int * cells;
+static int * buffer;
 
 int main();
 void init();
 void deinit();
 void initcurses();
 void tick();
+int neighbours(int y, int x);
 void add_circle(int y, int x, int radius);
 
 
-int main()
+int
+main()
 {
   init();
 
-  add_circle(LINES / 2, COLS / 2, (2 * LINES > COLS ? COLS / 2 : LINES) / 2);
+  add_circle(1 * LINES / 4, 1 * COLS / 4, (2 * LINES > COLS ? COLS / 4 : LINES) / 4);
+  add_circle(3 * LINES / 4, 1 * COLS / 4, (2 * LINES > COLS ? COLS / 4 : LINES) / 4);
+  add_circle(3 * LINES / 4, 3 * COLS / 4, (2 * LINES > COLS ? COLS / 4 : LINES) / 4);
+  add_circle(1 * LINES / 4, 3 * COLS / 4, (2 * LINES > COLS ? COLS / 4 : LINES) / 4);
+  add_circle(2 * LINES / 4, 2 * COLS / 4, (2 * LINES > COLS ? COLS / 4 : LINES) / 4);
 
-  tick();
-  getch();
+  do
+  {
+    tick();
+  }
+  while (getch() == 10);
 
   deinit();
 
   return 0;
 }
 
-void init()
+void
+init()
 {
   initcurses();
+
   cells = malloc(SIZE);
+  buffer = malloc(SIZE);
 }
 
-void initcurses()
+void
+initcurses()
 {
   initscr();
   curs_set(0);
@@ -49,38 +64,81 @@ void initcurses()
   keypad(stdscr, TRUE);
 }
 
-void deinit()
+void
+deinit()
 {
-  free(cells);
   endwin();
+
+  free(cells);
+  free(buffer);
 }
 
-void tick()
+void
+tick()
 {
-  int y, x;
+  int y, x, n;
 
-  for (y = 0; y < LINES; y++) {
-    for (x = 0; x < COLS; x++) {
-      if (CELL(y, x)) {
-        mvaddch(y, x, 'X');
+  memcpy(buffer, cells, SIZE);
+
+  for (y = 0; y < LINES; y++)
+  {
+    for (x = 0; x < COLS; x++)
+    {
+      n = neighbours(y, x);
+
+      if (n < 2 || n > 3)
+      {
+        BUFFER(y, x) = 0;
       }
+      if (n == 3)
+      {
+        BUFFER(y, x) = 1;
+      }
+
+      mvaddch(y, x, ALIVE(y, x) ? 'X' : ' ');
     }
   }
+
+  memcpy(cells, buffer, SIZE);
 
   refresh();
 }
 
-void add_circle(int y, int x, int radius)
+void
+add_circle(int y, int x, int radius)
 {
   int i;
 
   Point * points = malloc(sizeof(Point) * 360);
   get_points((Circle) {radius, (Point) {y, x}}, points);
 
-  for(i = 0; i < 360; i++)
+  for (i = 0; i < 360; i++)
   {
-    LIVE(points[i].y, points[i].x);
+    CELL(points[i].y, points[i].x) = 1;
   }
 
   free(points);
+}
+
+int
+neighbours(int y, int x)
+{
+  int dy, dx, n;
+
+  for (dy = -1; dy <= 1; dy++)
+  {
+    for (dx = -1; dx <= 1; dx++)
+    {
+      if (!(dy == 0 && dx == 0) &&
+          y + dy >= 0 &&
+          x + dx >= 0 &&
+          y + dy < LINES &&
+          x + dx < COLS)
+      {
+        n += ALIVE(y + dy, x + dx);
+      }
+    }
+  }
+
+  return n;
 }
